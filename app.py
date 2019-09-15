853b8a30448de884ac00b1c44480dd06ff786377
Rediet8abere <redietaberegesesse@gmail.com>
2019-09-15 14:06:29 -0700

    two routes :)

--- app.py
+++ app.py
@@ -5,16 +5,25 @@ import json
 app = Flask(__name__)
 
 @app.route('/')
-#@app.route('/index', methods=['GET', 'POST'])
 def index():
     """Return homepage."""
+
+    #An API call for trending GIFs
+    trending_response = requests.get("https://api.tenor.com/v1/trending?key=P7Z5S0UI3LE8&limit=10")
+    #Returns jason data
+    gifs_trending = json.loads(trending_response.content)
+
+    return render_template("index.html", gifs_trending_url_list=gifs_trending['results'])
+
+@app.route('/gifs')
+def gifsearch():
     # TODO: Extract query term from url
     search_term = request.args.get("q")
     # TODO: Make an API call to Tenor using the 'requests' library
     response = requests.get(f'https://api.tenor.com/v1/search?q={search_term}&key=P7Z5S0UI3LE8&limit=10')
     # TODO: Get the first 10 results from the search results
     gifs = json.loads(response.content)
-    
-    return render_template("index.html", gif_url_list=gifs['results'])
+    return render_template("base.html", gif_url_list=gifs['results'])
+
 if __name__ == '__main__':
     app.run(debug=True)
