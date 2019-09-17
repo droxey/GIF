279e55357d2cd79fe62a9ad7723db2a00772071c
Mario McGee <mario.mcgee@students.makeschool.com>
2019-09-17 10:33:48 -0700

    error message  added to base html

--- app.py
+++ app.py
@@ -13,18 +13,24 @@ def index():
     #Returns jason data
     gifs_trending = json.loads(trending_response.content)
 
-    return render_template("index.html", gifs_trending_url_list=gifs_trending['results'])
+    return render_template("index.html", gifs_url_list=gifs_trending['results'])
 
 @app.route('/gifs')
 def gifsearch():
     # TODO: Extract query term from url
     #"Request user input"
     search_term = request.args.get("q")
+   # if search_term != search_term.isalpha():
+    #    return False
     # TODO: Make an API call to Tenor using the 'requests' library
     response = requests.get(f'https://api.tenor.com/v1/search?q={search_term}&key=P7Z5S0UI3LE8&limit=10')
     # TODO: Get the first 10 results from the search results
     gifs = json.loads(response.content)
     return render_template("base.html", gif_url_list=gifs['results'])
 
+    
+
 if __name__ == '__main__':
     app.run(debug=True)
+
+
