e7b4d1aa977a2f851b7a46d592657a919c7381b4
liyaSileshi <liyasileshi@gmail.com>
2019-09-17 22:05:34 -0700

    trending button added

--- app.py
+++ app.py
@@ -13,9 +13,6 @@ def index():
 
     #Returns json data
     gifs_trending = json.loads(trending_response.content)
-
-
-
     return render_template("index.html", gif_url_list=gifs_trending['results'])
 
 @app.route('/gifs')
@@ -28,15 +25,16 @@ def gifsearch():
     gifs = json.loads(response.content)
     return render_template("base.html", gif_url_list=gifs['results'])
 
-# @app.route('/gifs')
-# def suggestion():
-#     # TODO: Extract query term from url
-#     search_term = request.args.get("q")
-#     # TODO: Make an API call to Tenor using the 'requests' library
-#     response = requests.get(f'https://api.tenor.com/v1/search?q={search_term}&key=P7Z5S0UI3LE8&limit=10')
-#     # TODO: Get the first 10 results from the search results
-#     gifs = json.loads(response.content)
-#     return render_template("base.html", gif_url_list=gifs['results'])
+@app.route('/')
+def trending():
+
+
+    #An API call for trending GIFs
+    trending_response = requests.get("https://api.tenor.com/v1/trending?key=P7Z5S0UI3LE8&limit=10")
+
+    #Returns json data
+    gifs_trending = json.loads(trending_response.content)
+    return render_template("index.html", gif_url_list=gifs_trending['results'])
 
 if __name__ == '__main__':
     app.run(debug=True)
