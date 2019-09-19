b256bf8298b9012d3b1c31fc3c6d82e6225b5924
Rediet8abere <redietaberegesesse@gmail.com>
2019-09-19 15:52:04 -0700

    css working

--- app.py
+++ app.py
@@ -10,10 +10,13 @@ def index():
 
     #An API call for trending GIFs
     trending_response = requests.get("https://api.tenor.com/v1/trending?key=P7Z5S0UI3LE8&limit=10")
-    #Returns jason data
+
+    #Returns json data
     gifs_trending = json.loads(trending_response.content)
 
-    return render_template("index.html", gifs_trending_url_list=gifs_trending['results'])
+
+
+    return render_template("index.html", gif_url_list=gifs_trending['results'])
 
 @app.route('/gifs')
 def gifsearch():
@@ -24,6 +27,16 @@ def gifsearch():
     # TODO: Get the first 10 results from the search results
     gifs = json.loads(response.content)
     return render_template("base.html", gif_url_list=gifs['results'])
+
+# @app.route('/gifs')
+# def suggestion():
+#     # TODO: Extract query term from url
+#     search_term = request.args.get("q")
+#     # TODO: Make an API call to Tenor using the 'requests' library
+#     response = requests.get(f'https://api.tenor.com/v1/search?q={search_term}&key=P7Z5S0UI3LE8&limit=10')
+#     # TODO: Get the first 10 results from the search results
+#     gifs = json.loads(response.content)
+#     return render_template("base.html", gif_url_list=gifs['results'])
 
 if __name__ == '__main__':
     app.run(debug=True)
