1b3595f1890748bf39ec4af6e874f3dfc78edcb6
liyaSileshi <liyasileshi@gmail.com>
2019-09-18 18:33:02 -0700

    added button random

--- app.py
+++ app.py
@@ -25,16 +25,16 @@ def gifsearch():
     gifs = json.loads(response.content)
     return render_template("base.html", gif_url_list=gifs['results'])
 
-@app.route('/')
-def trending():
-
-
+@app.route('/random')
+def random():
+    """Return homepage."""
+    search_term = request.args.get("q")
     #An API call for trending GIFs
-    trending_response = requests.get("https://api.tenor.com/v1/trending?key=P7Z5S0UI3LE8&limit=10")
+    random_response = requests.get(f'https://api.tenor.com/v1/random?q={search_term}&key=P7Z5S0UI3LE8&limit=10')
 
     #Returns json data
-    gifs_trending = json.loads(trending_response.content)
-    return render_template("index.html", gif_url_list=gifs_trending['results'])
+    gifs_random = json.loads(random_response.content)
+    return render_template("base.html", gif_url_list=gifs_random['results'])
 
 if __name__ == '__main__':
     app.run(debug=True)
