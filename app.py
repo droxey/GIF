1758ae92a1a613b4e38bb33bb5a6157e7a54ce3f
Rediet8abere <redietaberegesesse@gmail.com>
2019-09-19 17:18:11 -0700

    three routes :)

--- app.py
+++ app.py
@@ -13,10 +13,7 @@ def index():
 
     #Returns json data
     gifs_trending = json.loads(trending_response.content)
-
-
-
-    return render_template("index.html", gif_url_list=gifs_trending['results'])
+    return render_template("base.html", gif_url_list=gifs_trending['results'])
 
 @app.route('/gifs')
 def gifsearch():
@@ -28,15 +25,15 @@ def gifsearch():
     gifs = json.loads(response.content)
     return render_template("base.html", gif_url_list=gifs['results'])
 
-# @app.route('/gifs')
-# def suggestion():
-#     # TODO: Extract query term from url
-#     search_term = request.args.get("q")
-#     # TODO: Make an API call to Tenor using the 'requests' library
-#     response = requests.get(f'https://api.tenor.com/v1/search?q={search_term}&key=P7Z5S0UI3LE8&limit=10')
-#     # TODO: Get the first 10 results from the search results
-#     gifs = json.loads(response.content)
-#     return render_template("base.html", gif_url_list=gifs['results'])
+@app.route('/random')
+def random():
+   """Return homepage."""
+   search_term = request.args.get("q")
+   #An API call for trending GIFs
+   random_response = requests.get(f'https://api.tenor.com/v1/random?q={search_term}&key=P7Z5S0UI3LE8&limit=10')
+   #Returns json data
+   gifs_random = json.loads(random_response.content)
+   return render_template("base.html", gif_url_list=gifs_random['results'])
 
 if __name__ == '__main__':
     app.run(debug=True)
