b985198a4efadb49ffb67310b3d79fd2db7b1de7
Rediet8abere <redietaberegesesse@gmail.com>
2019-09-19 18:52:13 -0700

    comments and doc string

--- app.py
+++ app.py
@@ -2,38 +2,45 @@ from flask import Flask, render_template, request, url_for
 import requests
 import json
 
+
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    """Return homepage."""
-
-    #An API call for trending GIFs
+    """Return trending gifs to the homepage."""
+    # gets a response from the API
     trending_response = requests.get("https://api.tenor.com/v1/trending?key=P7Z5S0UI3LE8&limit=10")
-
-    #Returns json data
+    # returns a dictionary of gif data
     gifs_trending = json.loads(trending_response.content)
+    # renders base html file and passes a dictionary of extracted trending gif results as a parameter
     return render_template("base.html", gif_url_list=gifs_trending['results'])
 
 @app.route('/gifs')
 def gifsearch():
-    # TODO: Extract query term from url
+    """Returns gifs based on the search query the user enters """
+    # requests a query string from the user
     search_term = request.args.get("q")
-    # TODO: Make an API call to Tenor using the 'requests' library
+    # gets a response from the API
     response = requests.get(f'https://api.tenor.com/v1/search?q={search_term}&key=P7Z5S0UI3LE8&limit=10')
-    # TODO: Get the first 10 results from the search results
+    # returns a dictionary of gif data
     gifs = json.loads(response.content)
+    # renders base html file and passes a dictionary of extracted gifs results as a parameter
     return render_template("base.html", gif_url_list=gifs['results'])
+
+
 
 @app.route('/random')
 def random():
-   """Return homepage."""
+   """Return random gifs when the user hit the randon button."""
+   # requests a query string from the user
    search_term = request.args.get("q")
-   #An API call for trending GIFs
+   # gets a response from the API
    random_response = requests.get(f'https://api.tenor.com/v1/random?q={search_term}&key=P7Z5S0UI3LE8&limit=10')
-   #Returns json data
+   # returns a dictionary of gif data
    gifs_random = json.loads(random_response.content)
+   # renders base html file and passes a dictionary of extracted random gif results as a parameter)
    return render_template("base.html", gif_url_list=gifs_random['results'])
 
+# a scope where the app is run
 if __name__ == '__main__':
     app.run(debug=True)
