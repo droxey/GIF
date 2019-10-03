2b2e8b0783df08950e5efad7b07f85cd1fde5abb
Mario McGee <mario.mcgee@students.makeschool.com>
2019-10-03 14:58:19 -0700

    pushed to git hub

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from flask import Flask, render_template, request, url_for
 import requests
 import json
+import os
+from dotenv import load_dotenv
+load_dotenv()
+TENOR_API_KEY = os.getenv("TENOR_API_KEY")
+
 
 app = Flask(__name__)
 
@@ -9,11 +14,11 @@ def index():
     """Return homepage."""
 
     #An API call for trending GIFs
-    trending_response = requests.get("https://api.tenor.com/v1/trending?key=P7Z5S0UI3LE8&limit=10")
+    trending_response = requests.get(f"https://api.tenor.com/v1/trending?key={TENOR_API_KEY}&limit=10")
     #Returns jason data
     gifs_trending = json.loads(trending_response.content)
 
-    return render_template("index.html", gifs_url_list=gifs_trending['results'])
+    return render_template("index.html", gif_url_list=gifs_trending['results'])
 
 @app.route('/gifs')
 def gifsearch():
